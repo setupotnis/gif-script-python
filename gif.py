38ec59ab411c1257996f292345e78c55f5c48c70
Setu Potnis <46113818+setupotnis@users.noreply.github.com>
2019-07-31 13:59:45 -0400

    finished video to mp4 converter

--- gif.py
+++ gif.py
@@ -2,7 +2,20 @@
 import imageio
 import os
 
-clip = os.path.abspath('sc-vid-to-gif.mp4')
+clip = os.path.abspath('file-')
 
 def gifmaker(inputPath, targetFormat):
     outputPath = os.path.splitext(inputPath)[0] + targetFormat
+
+    print(f'converting {inputPath} \n to {outputPath}')
+    reader = imageio.get_reader(inputPath)
+    fps = reader.get_meta_data()['fps']
+    writer = imageio.get_writer(outputPath, fps=fps)
+
+    for frames in reader:
+        writer.append_data(frames)
+        print(f'Frame {frames}')
+    print('Done')
+    writer.close()
+
+gifmaker(clip, 'insert-targetFormat')
